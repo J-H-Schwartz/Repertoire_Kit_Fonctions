d20bd2da5b8b06d75f518251741d9cd55b5b94b0
Jonathan <schwartz.jo@laposte.net>
2020-01-22 09:16:42 +0100

    Refactorisation du code, ajout des tests en majuscules pour que l'utilisateur puisse enregistrer les noms avec la casse qu'il souhaite, et renommage des variables dans les fonctions.

--- repertoire_ui.py
+++ repertoire_ui.py
@@ -1,15 +1,19 @@
 # coding: utf-8
-from repertoire_utils.repertoire_action import verification_existance_contact, ajouter_numero, \
-    modification_numero, recherche_contact, supprimer_numero
+from repertoire_utils.repertoire_action import verification_existance_contact, ajouter_personne, \
+    modification_numero, chercher_personne, supprimer_personne
 from terminaltables import DoubleTable
 
-repertoire = [{'NOM': 'MICHAEL', 'NUMERO': '1111111111', 'ADRESSE': 'blabla@mfloe.com'},
-              {'NOM': 'LINDA', 'NUMERO': '2222222222', 'ADRESSE': 'blbblb@zvrez.com'},
-              {'NOM': 'SANDRA', 'NUMERO': '3333333333', 'ADRESSE': 'fzefzzcz@fzv.com'},
-              {'NOM': 'SANDRA2', 'NUMERO': '4444444444', 'ADRESSE': 'fzgzgvdsz@fzv.com'},
-              {'NOM': 'SANDRA3', 'NUMERO': '555555555', 'ADRESSE': 'aaeaczzcz@fzv.com'}]
+repertoire = [{'NOM': 'michael', 'NUMERO': '1111111111', 'ADRESSE': 'blabla@mfloe.com', 'NOM_UPPER': "MICHAEL"},
+              {'NOM': 'Linda', 'NUMERO': '2222222222', 'ADRESSE': 'blbblb@zvrez.com', 'NOM_UPPER': "LINDA"},
+              {'NOM': 'SANDRA', 'NUMERO': '3333333333', 'ADRESSE': 'fzefzzcz@fzv.com', 'NOM_UPPER': "SANDRA"},
+              {'NOM': 'sandra2', 'NUMERO': '4444444444', 'ADRESSE': 'fzgzgvdsz@fzv.com', 'NOM_UPPER': "SANDRA2"},
+              {'NOM': 'Sandra3', 'NUMERO': '555555555', 'ADRESSE': 'aaeaczzcz@fzv.com', 'NOM_UPPER': "SANDRA3"}]
 resultat = []
 
+
+# Fonction tri alphabetique
+# def tri_upper(key='NOM_UPPER'):
+#    return 'NOM_UPPER'
 
 # Fonction Afficher_Repertoire
 
@@ -29,16 +33,16 @@ def afficher_repertoire(repertoire):
 
 def ajout_contact(repertoire):
     while True:
-        nom = input("Entrez le nom du contact à ajouter ou tapez Entrée pour revenir au menu principal: ").upper()
+        nom = input("Entrez le nom du contact à ajouter ou tapez Entrée pour revenir au menu principal: ")
         resultat = verification_existance_contact(repertoire, nom)
         if resultat:
             print("Ce contact existe déja.")
             continue
         if nom == "":
             continue
-        numero = input("Entrez le numéro du contact à ajouter: ").upper()
+        numero = input("Entrez le numéro du contact à ajouter: ")
         adresse = input("Entrez l'adresse du contact à ajouter: ")
-        ajouter_numero(nom, numero, adresse, repertoire)
+        ajouter_personne(nom, numero, adresse, repertoire)
         print("Le contact a bien été ajouté.")
         return
 
@@ -46,10 +50,10 @@ def ajout_contact(repertoire):
 # Fonction Modification de contact
 
 def modification(repertoire):
-    destination = ""
+    champ = ""
     while True:
         modification = ""
-        nom = input("Entrez le nom du contact à modifier ou tapez Entrée pour revenir au menu précédent: ").upper()
+        nom = input("Entrez le nom du contact à modifier ou tapez Entrée pour revenir au menu précédent: ")
         resultat = verification_existance_contact(repertoire, nom)
         if nom == "":
             print("Retour au menu précédent.")
@@ -58,20 +62,20 @@ def modification(repertoire):
             print("Ce contact n'existe pas.")
             continue
         else:
-            destination = input("Que voulez-vous modifier ? (N)uméro / (A)dresse: ").upper()
-        if destination == "N":
-            modification = input("Entrez le nouveau numéro: ").upper()
-            destination = 'NUMERO'
+            champ = input("Que voulez-vous modifier ? (N)uméro / (A)dresse: ")
+        if champ.upper() == "N":
+            modification = input("Entrez le nouveau numéro: ")
+            champ = 'NUMERO'
             break
-        elif destination == "A":
+        elif champ.upper() == "A":
             modification = input("Entrez la nouvelle adresse: ")
-            destination = 'ADRESSE'
+            champ = 'ADRESSE'
             break
         else:
             print("Commande non reconnue. Recommencez.")
             continue
     try:
-        resultat = modification_numero(repertoire, nom, destination, modification)
+        resultat = modification_numero(repertoire, nom, champ, modification)
         if resultat:
             print("Le contact a été modifié.")
             return
@@ -87,20 +91,21 @@ def modification(repertoire):
 def recherche_de_contact(repertoire):
     resultat = []
     while True:
-        choix = input("Rechercher par : (N)om  /  Numero(0) / (P)récédent").upper()
-        if choix == "0" or choix == "N":
-            champ = input("Entrez votre recherche: ").upper()
-            if choix == "0":
+        choix = input("Rechercher par : (N)om  /  Numero(0) / (P)récédent")
+        if choix.upper() == "0" or choix.upper() == "N":
+            champ = input("Entrez votre recherche: ")
+            argument = {}
+            if choix.upper() == "0":
                 argument = {"numero": champ}
-            if choix == "N":
+            if choix.upper() == "N":
                 argument = {"nom": champ}
             if champ == "":
                 print("Commande non reconnue. Recommencez")
                 continue
-            resultat = recherche_contact(repertoire=repertoire,
+            resultat = chercher_personne(repertoire=repertoire,
                                          type_de_recherche=choix, **argument)
             return resultat
-        elif choix == "P":
+        elif choix.upper() == "P":
             return resultat
         else:
             print("Commande non reconnue. Recommencez.")
@@ -110,10 +115,10 @@ def recherche_de_contact(repertoire):
 
 def supprimer(repertoire):
     while True:
-        nom = input("Entrez le nom du contact à supprimer ou tapez Entrée pour revenir au menu: ").upper()
+        nom = input("Entrez le nom du contact à supprimer ou tapez Entrée pour revenir au menu: ")
         if nom == "":
             return
-        resultat = supprimer_numero(repertoire, nom)
+        resultat = supprimer_personne(repertoire, nom)
         if resultat:
             print("Le contact a été supprimé.")
             return
@@ -121,7 +126,7 @@ def supprimer(repertoire):
             print("Ce contact n'existe pas.")
 
 
-# Début.
+# Fonction principale du répertoire.
 
 def main():
     while True:
@@ -130,22 +135,22 @@ def main():
 
         # Afficher répertoire.
 
-        if choix == "L":
+        if choix.upper() == "L":
             afficher_repertoire(repertoire)
 
         # Ajouter contact.
 
-        elif choix == "A":
+        elif choix.upper() == "A":
             ajout_contact(repertoire)
 
         # Modification.
 
-        elif choix == "M":
+        elif choix.upper() == "M":
             modification(repertoire)
 
         # Recherche contact.
 
-        elif choix == "R":
+        elif choix.upper() == "R":
             resultat = recherche_de_contact(repertoire)
             if len(resultat) < 1:
                 print("La recherche n'a donné aucun résultat.")
@@ -155,12 +160,12 @@ def main():
 
         # Suppression contact.
 
-        elif choix == "S":
+        elif choix.upper() == "S":
             supprimer(repertoire)
 
         # Quitter programme.
 
-        elif choix == "Q":
+        elif choix.upper() == "Q":
             print("Programme quitté.")
             break
 
--- repertoire_utils/repertoire_action.py
+++ repertoire_utils/repertoire_action.py
@@ -5,21 +5,21 @@
 
 def verification_existance_contact(repertoire, nom):
     for item in repertoire:
-        if nom == item['NOM']:
+        if nom.upper() == item['NOM'].upper():
             return True
 
 
 # Fonction Ajouter numéro
 
-def ajouter_numero(nom, numero, adresse, repertoire):
-    repertoire.append({"NOM": nom, "NUMERO": numero, "ADRESSE": adresse})
+def ajouter_personne(nom, numero, adresse, repertoire):
+    repertoire.append({"NOM": nom, "NUMERO": numero, "ADRESSE": adresse, "NOM_UPPER": nom.upper()})
 
 
 # Fonction Suppression numéro
 
-def supprimer_numero(repertoire, nom):
+def supprimer_personne(repertoire, nom):
     for contact, element in enumerate(repertoire):
-        if nom == element['NOM']:
+        if nom.upper() == element['NOM'].upper():
             del repertoire[contact]
             return True
     return False
@@ -27,18 +27,18 @@ def supprimer_numero(repertoire, nom):
 
 # Fonction Recherche
 
-def recherche_contact(repertoire, nom="defaut", numero="defaut", type_de_recherche="defaut"):
+def chercher_personne(repertoire, nom="defaut", numero="defaut", type_de_recherche="defaut"):
     contact_trouve = []
     champ = ''
     entree_recherchee = ""
     for item in repertoire:
-        if type_de_recherche == "N":
+        if type_de_recherche.upper() == "N":
             champ = 'NOM'
             entree_recherchee = nom
-        elif type_de_recherche == "0":
+        elif type_de_recherche.upper() == "0":
             champ = 'NUMERO'
             entree_recherchee = numero
-        if entree_recherchee in item[champ]:
+        if entree_recherchee.upper() in item[champ].upper():
             contact_trouve.append({'NOM': item['NOM'], 'NUMERO': item
                                    ['NUMERO'], 'ADRESSE': item['ADRESSE']})
     return contact_trouve
@@ -48,8 +48,9 @@ def recherche_contact(repertoire, nom="defaut", numero="defaut", type_de_recherc
 
 def modification_numero(repertoire, contact, destination, modification):
     for contacts in repertoire:
-        if contact == contacts['NOM']:
+        if contact.upper() == contacts['NOM'].upper():
             contacts[destination] = modification
+            contacts['NOM_UPPER'] = modification.upper()
             return True
     return False
 
